062746647d13faafb42bf91f8e7132f56117b7a1
josefr.almeidaads <jose.almeida@i476.com.br>
2021-08-25 16:59:59 -0300

    app finalizado

--- android/app/src/main/java/com/nativeelements/MainActivity.java
+++ android/app/src/main/java/com/nativeelements/MainActivity.java
@@ -1,9 +1,15 @@
 package com.nativeelements;
 
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen; // Adicione esse import
+import android.os.Bundle; // Adicione esse import
 
 public class MainActivity extends ReactActivity {
-
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
+    }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
